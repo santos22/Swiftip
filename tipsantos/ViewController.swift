adc2abb972c17f68307e628e1d11c188600135c8
santos22 <santosjavier22@gmail.com>
2015-12-30 00:22:30 -0600

    Changed background color for the app temporarily

--- tipsantos/ViewController.swift
+++ tipsantos/ViewController.swift
@@ -19,6 +19,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.lightGrayColor()
         tipLabel.text = "$0.00"
         totalLabel.text = "$0.00"
     }
