2bf09a518a14c528cb969c0e620115c77c67414e
santos22 <santosjavier22@gmail.com>
2015-12-31 05:43:07 -0600

    Added icons to the app

---
+++ tipsantos/BillHistory.swift
@@ -0,0 +1,13 @@
+//
+//  BillHistory.swift
+//  tipsantos
+//
+//  Created by Santos Solorzano on 12/31/15.
+//  Copyright © 2015 santosjs. All rights reserved.
+//
+
+import UIKit
+
+class BillHistory: NSObject {
+
+}
